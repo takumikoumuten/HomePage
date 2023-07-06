252c23472cd411696db8abda635705bb69e9cc56
takumikoumuten <takumikoumutenjp@gmail.com>
2023-07-06 19:53:52 +0900

    fix: アクセスマップ修正

--- src/components/accessMap.tsx
+++ src/components/accessMap.tsx
@@ -5,6 +5,36 @@ import { Status, Wrapper } from "@googlemaps/react-wrapper"
 import Spinner from "./spinner"
 import { consts } from "../consts"
 
+const PlaceCard = ({ address }: { address: string }) => {
+  const googleMapsQuery = address
+
+  return (
+    <div className="bg-white shadow overflow-hidden sm:rounded py-5 px-5">
+      <div className="grid grid-cols-1 gap-3">
+        <div className="col-span-1">
+          <h3 className="text-lg leading-6 font-medium text-gray-900">
+            Location
+          </h3>
+          <p className="mt-1 text-sm text-gray-500">{address}</p>
+        </div>
+        <div className="col-span-1 grid">
+          <div className="grid place-items-center grid-flow-row place-self-end">
+            <a
+              href={`https://www.google.com/maps/search/?api=1&query=${googleMapsQuery}`}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-indigo-600 hover:text-indigo-900"
+            >
+              <span className="material-symbols-outlined text-3xl">map</span>
+            </a>
+            <div className="text-[0.75rem] -mt-1 text-indigo-600 opacity-80">経路</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
 const MapComponent = ({ apiKey }: { apiKey: string }): React.ReactElement => {
   const [center, setCenter] = useState<google.maps.LatLngLiteral | null>(null)
   const [error, setError] = useState<string | null>(null)
@@ -24,7 +54,12 @@ const MapComponent = ({ apiKey }: { apiKey: string }): React.ReactElement => {
   }
 
   return center ? (
-    <MyMapComponent center={center} zoom={15} />
+    <div className="grid relative">
+      <MyMapComponent center={center} zoom={18} />
+      <div className="absolute top-2 left-2">
+        <PlaceCard address={consts.住所} />
+      </div>
+    </div>
   ) : (
     <div>centerが存在しない</div>
   )
@@ -48,7 +83,6 @@ const fetchLocation = async (
   return data.results[0].geometry.location
 }
 
-// 地図を表示するコンポーネント
 function MyMapComponent({
   center,
   zoom,
@@ -57,13 +91,27 @@ function MyMapComponent({
   zoom: number
 }) {
   const ref = useRef(null)
+  const markerRef = useRef<google.maps.Marker | null>(null)
 
   useEffect(() => {
-    ref.current &&
-      new google.maps.Map(ref.current, {
+    if (ref.current) {
+      const map = new google.maps.Map(ref.current, {
         center,
         zoom,
       })
+
+      markerRef.current = new google.maps.Marker({
+        position: center,
+        map,
+        title: "Center",
+      })
+    }
+
+    return () => {
+      if (markerRef.current) {
+        markerRef.current.setMap(null)
+      }
+    }
   }, [center, zoom])
 
   return <div ref={ref} id="map" className="aspect-[4/3] w-full" />
